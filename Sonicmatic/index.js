0ae8688e3536b248138a8e70b2f9fa1cd1fc4992
mespn <manuelen2002@gmail.com>
2024-04-01 01:27:46 -0500

    Filled in feature titles and description

--- Sonicmatic/index.js
+++ Sonicmatic/index.js
@@ -24,6 +24,25 @@ function showFeatures(){
 }
 
 function videoCard(index, thing){
+  url = thing.videoURL
+  if (thing.videoURL == ""){
+    thing.videoURL = `
+    <svg
+  class="bd-placeholder-img card-img-top"
+  width="100%"
+  height="180"
+  xmlns="http://www.w3.org/2000/svg"
+  role="img"
+  aria-label="Placeholder: Image cap"
+  preserveAspectRatio="xMidYMid slice"
+  focusable="false"
+>
+  <title>Placeholder</title>
+  <rect width="100%" height="100%" fill="#868e96" />
+  <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Placeholder</text>
+</svg>
+`
+  }
   return `
   <div class="col">
     <div class="card h-100">
